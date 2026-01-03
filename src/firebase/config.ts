2f80bed82e9e4d9c2a6858991f77c4775b59cb40
Pk Bhai <pb6620113@gmail.com>
2026-01-03 11:22:38 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:72632837630:web:980659b633d0cab4930caf",
   "apiKey": "AIzaSyAXpF2WMHHqJATlCRi-UIs3zomaizX9sFo",
   "authDomain": "studio-9941308746-66959.firebaseapp.com",
+  "storageBucket": "studio-9941308746-66959.appspot.com",
   "measurementId": "",
   "messagingSenderId": "72632837630"
 };
